696d560afba39f3172e4fe8fcfd9dc8142cde00c
Adeleye Tolulope <tolulope@grit.systems>
2019-03-22 22:45:26 +0100

    Updates

---
+++ precache-manifest.9ae2ca537880d70893dd7df20d2e7e9f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e21159ecb44050b0e05f",
+    "url": "/Motadeh/food-delivery/static/css/main.b6c452dd.chunk.css"
+  },
+  {
+    "revision": "e21159ecb44050b0e05f",
+    "url": "/Motadeh/food-delivery/static/js/main.e21159ec.chunk.js"
+  },
+  {
+    "revision": "b74a851775b503b1253c",
+    "url": "/Motadeh/food-delivery/static/js/runtime~main.b74a8517.js"
+  },
+  {
+    "revision": "3a4ca02afe824ed5b1d0",
+    "url": "/Motadeh/food-delivery/static/js/2.3a4ca02a.chunk.js"
+  },
+  {
+    "revision": "6649d87c2d27097262b96a1b255aa7dc",
+    "url": "/Motadeh/food-delivery/static/media/Ford-Transit-1.6649d87c.png"
+  },
+  {
+    "revision": "86575572ae18e8ed06b2b455abcb4e7c",
+    "url": "/Motadeh/food-delivery/static/media/1261.86575572.png"
+  },
+  {
+    "revision": "f5d8a18f3c1720221d0a6f87eef7831a",
+    "url": "/Motadeh/food-delivery/static/media/Bukka-Hut-Logo.f5d8a18f.png"
+  },
+  {
+    "revision": "8777696e744715e6d92698b326e504ca",
+    "url": "/Motadeh/food-delivery/static/media/01.8777696e.png"
+  },
+  {
+    "revision": "da20ceb15d87e379cdbd4022fe2622b1",
+    "url": "/Motadeh/food-delivery/static/media/580b57fcd9996e24bc43c1a8.da20ceb1.png"
+  },
+  {
+    "revision": "7a34698a64b229466fa318fae112cd4f",
+    "url": "/Motadeh/food-delivery/static/media/tgb-express-1.7a34698a.png"
+  },
+  {
+    "revision": "7221ff0bf2145fef97eacb1fb2ca5402",
+    "url": "/Motadeh/food-delivery/static/media/9266-2019-toyota-corolla.7221ff0b.png"
+  },
+  {
+    "revision": "73443feb3798857581b235f6cf2929e4",
+    "url": "/Motadeh/food-delivery/static/media/Mask_Group_52.73443feb.png"
+  },
+  {
+    "revision": "a3e25059940b9715928decaafd10c17e",
+    "url": "/Motadeh/food-delivery/index.html"
+  }
+];
