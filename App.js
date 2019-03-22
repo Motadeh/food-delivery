baa2a33876d32ceed18c1c3495c9fe73a7b44ca5
Adeleye Tolulope <tolulope@grit.systems>
2019-03-22 21:53:14 +0100

    app.js moved

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from "./Component/Header/Header"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
